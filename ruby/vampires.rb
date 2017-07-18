758c5df4d2962752ae3dbfd9336a0a9725170629
echo 'hello world <ssmyth1071@gmail.com>
2017-07-18 13:21:39 -0400

    complete release 0 gather data on vampire employee

---
+++ ruby/vampires.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+employee_name = gets.chomp
+puts "How old are you?"
+employee_age = gets.chomp
+puts "What year were you born?"
+employee_birth_year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+employee_like_garlic = gets.chomp
+puts "Would you like to enroll in the company’s health insurance? "
+employee_health_insurance = gets.chomp
