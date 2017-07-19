5658d1a9decf6dcdd7e08aa4835280b0e19ecbf7
echo 'hello world <ssmyth1071@gmail.com>
2017-07-18 22:06:00 -0400

    complete release 3 and 4. still need to work on the logic for release 2

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,25 +1,37 @@
-puts "What is your name?"
-employee_name = gets.chomp
-puts "How old are you?"
-employee_claim_age = gets.chomp.to_i
-puts "What year were you born?"
-employee_birth_year = gets.chomp.to_i
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-employee_like_garlic = gets.chomp
-puts "Would you like to enroll in the company’s health insurance? "
-employee_health_insurance = gets.chomp
-employee_math_age = 2017 - employee_birth_year
-	if ((employee_math_age) == (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) 
-		&& (employee_like_garlic == "yes" || employee_health_insurance == "yes")
-		puts "Probably not a vampire."	
-	elsif ((employee_math_age) != (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) 
-		&& (employee_like_garlic == "no" || employee_health_insurance == "no")
-		puts "Probably a vampire."		
-	elsif ((employee_math_age) != (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) 
-		&& employee_like_garlic == "no" && employee_health_insurance == "no"
-		puts "Almost certainly a vampire."
-	elsif employee_name == "Drake Cula" || employee_name == "Tu Fang" || employee_name == "Edward Cullen"
-		puts "Definitely a vampire."		
-	else
-	  puts "Results inconclusive."
-	end
+puts "How many employees will you be processing?"
+employees_processed = gets.chomp.to_i
+
+completed_process = 0 
+ until completed_process == employees_processed
+
+    puts "What is your name?"
+    employee_name = gets.chomp
+    puts "How old are you?"
+    employee_claim_age = gets.chomp.to_i
+    puts "What year were you born?"
+    employee_birth_year = gets.chomp.to_i
+    puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+    employee_like_garlic = gets.chomp
+    puts "Would you like to enroll in the company’s health insurance? "
+    employee_health_insurance = gets.chomp
+    employee_math_age = 2017 - employee_birth_year
+    allergies = "" 
+     while allergies != "sunshine" 
+      puts "List any allergies: (Type done when complete)"
+      allergies = gets.chomp
+      break if allergies == "done"
+    end
+    completed_process +=1 
+    case
+    	when ((employee_math_age) == (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) && (employee_like_garlic == "yes" || employee_health_insurance == "yes")
+    		puts "Probably not a vampire."	
+    	when ((employee_math_age) != (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) && (employee_like_garlic == "no" || employee_health_insurance == "no")
+    		  puts "Probably a vampire."		
+      	when ((employee_math_age) != (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) && employee_like_garlic == "no" && employee_health_insurance == "no"
+    		      puts "Almost certainly a vampire."
+    	when  employee_name == "Drake Cula" || employee_name == "Tu Fang" || employee_name == "Edward Cullen"
+    		        puts "Definitely a vampire."		
+    	else
+    	          puts "Results inconclusive."
+      end
+   end
