a80f3dd8a7253e3316e2be49f865297116fbb22c
echo 'hello world <ssmyth1071@gmail.com>
2017-07-19 19:08:11 -0400

    create encrypt method to advance every letter of a string forward one ltter

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,28 @@
+=begin
+Algo for Encrypt
+1. accept a string as input
+2. index each letter in the string 
+3. take first letter in the string
+4. change that letter to the next letter sequentially in the alphabet (.next function)
+5. repeat  for each additional letter in the string - loop steps 2 through 4
+
+Algo for Decrypt
+1. accept a string as input
+2. index each letter in the string 
+3. take first letter in the string
+4. change that letter to the next letter sequentially in the alphabet. 
+	-Logic to achieve ??  
+	-index the whole alphabet
+	- assign each letter to the alphabet index. subtract one from the assignment
+5. repeat  for each additional letter in the string - loop steps 2 through 4
+=end
+
+def encrypt(string)
+  count = 0
+  while count < string.length
+    string[count] = string[count].next
+    count += 1
+  end
+  return string
+end
+puts encrypt("abc")
