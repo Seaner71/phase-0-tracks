48f04ccf520203c671dd23484cee7b4f21899603
echo 'hello world <ssmyth1071@gmail.com>
2017-07-20 12:16:19 -0400

    update case statement. issue with checking last condition

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -26,17 +26,16 @@ completed_process = 0
     end
     completed_process +=1 
     case
-      when  employee_name == "Drake Cula" || employee_name == "Tu Fang" || employee_name == "Edward Cullen"
-                puts "Definitely a vampire."
-      when ((employee_math_age) == (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) && (employee_like_garlic == "yes" || employee_health_insurance == "yes")
+       when ((employee_math_age) == (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) && (employee_like_garlic == "yes" || employee_health_insurance == "yes")
         puts "Probably not a vampire."  
       when ((employee_math_age) != (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) && employee_like_garlic == "no" && employee_health_insurance == "no"
         puts "Almost certainly a vampire."
       when ((employee_math_age) != (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) && (employee_like_garlic == "no" || employee_health_insurance == "no")
          puts "Probably a vampire." 
-              
+      when  employee_name == "Drake Cula" || employee_name == "Tu Fang" || employee_name == "Edward Cullen"
+                puts "Definitely a vampire."
       else
                 puts "Results inconclusive."
       end
-   end
+  end
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
