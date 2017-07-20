af3ea44a7dfeecd9a16ab777b9d913c2b5bfa6c9
echo 'hello world <ssmyth1071@gmail.com>
2017-07-20 11:46:38 -0400

    complete 4.6 with driver code and edge case logic

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -21,11 +21,11 @@ def encrypt(string)
   count = 0
   
   while count < string.length
-  if string[count] =="z" # add edge case logic to overcome z.next = aa
+  if string[count] =="z" # add edge case logic to overcome z next = aa
       string[count] = "a"
   else
     string[count] = string[count].next # increment forward one letter
-  end   
+ end   
     count += 1
   end
   return string
@@ -36,15 +36,35 @@ def decrypt(string)
   alphabet = "abcdefghijklmnopqrstuvwxyz" # set variable to alphabet for readability
   count = 0
   while count < string.length
-  string[count] =  alphabet[alphabet.index(string[count])-1] # get letter's position in alphabet -1 (convert letter to index# -1) then convert index# back to a letter 
+  string[count] =  alphabet[alphabet.index(string[count])-1] # get letter's position in alphabet -1 (convert letter to index#) then convert index# back to new letter 
    count += 1
   end
   return string
 end
 
+puts "Would you like to decrypt or encrypt a password?"
+passtype = gets.chomp
+loop = true
+while loop == true
+  if passtype == "encrypt"
+    loop = false
+    puts "What's the password?"
+    puts encrypt(gets.chomp)
+  elsif passtype == "decrypt"
+    loop = false
+    puts "What's the password?"
+    puts decrypt(gets.chomp)
+  else
+    loop = true
+    puts "Would you like to decrypt or encrypt a password?"
+    passtype = gets.chomp
+  end
+end
 
-puts encrypt("abc")
-puts encrypt("zed")
-puts decrypt("bcd")
-puts decrypt("afe")
-puts decrypt(encrypt("swordfish"))
+# puts decrypt("hello")
+
+# puts encrypt("abc")
+# puts encrypt("zed")
+# puts decrypt("bcd")
+# puts decrypt("afe")
+# puts decrypt(encrypt("swordfish"))
