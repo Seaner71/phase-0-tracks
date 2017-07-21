ac20f33bfaf3c001394e3fa4faf6d08b78cabe42
echo 'hello world <ssmyth1071@gmail.com>
2017-07-21 18:42:34 -0400

    make logic work but not in order asked. time box expired

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -14,28 +14,26 @@ completed_process = 0
     employee_like_garlic = gets.chomp
     puts "Would you like to enroll in the company’s health insurance? "
     employee_health_insurance = gets.chomp
-    employee_math_age = 2017 - employee_birth_year
-    allergies = "" 
-     while allergies != "sunshine" 
-      puts "List any allergies: (Type done when complete)"
+    employee_math_age = Time.new.year - employee_birth_year
+    puts "List any allergies: (Type done when complete)"
       allergies = gets.chomp
-        if allergies == "sunshine"
-          puts "Probably a vampire."
-        end
-      break if allergies == "done"
-    end
+    until allergies == "sunshine" || allergies == "done"
+      allergies = gets.chomp
+     end
     completed_process +=1 
     case
-       when ((employee_math_age) == (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) && (employee_like_garlic == "yes" || employee_health_insurance == "yes")
-        puts "Probably not a vampire."  
-      when ((employee_math_age) != (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) && employee_like_garlic == "no" && employee_health_insurance == "no"
-        puts "Almost certainly a vampire."
-      when ((employee_math_age) != (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) && (employee_like_garlic == "no" || employee_health_insurance == "no")
-         puts "Probably a vampire." 
       when  employee_name == "Drake Cula" || employee_name == "Tu Fang" || employee_name == "Edward Cullen"
-                puts "Definitely a vampire."
+        puts "Definitely a vampire."
+      when ((employee_math_age) == (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) && (employee_like_garlic == "yes" || employee_health_insurance == "yes")
+        puts "Probably not a vampire."
+      when ((employee_math_age) != (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) && employee_like_garlic == "no" && employee_health_insurance == "no"
+        puts "Almost certainly a vampire."  
+      when ((employee_math_age) != (employee_claim_age) || employee_math_age == (employee_claim_age + 1)) && (employee_like_garlic == "no" || employee_health_insurance == "no")
+        puts "Probably a vampire."    
+      when alergies == "sunlight"
+        puts "Probably a vampire"
       else
-                puts "Results inconclusive."
+        puts "Results inconclusive."
       end
-  end
+   end
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
