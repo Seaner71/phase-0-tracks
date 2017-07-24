08c8025ba176cc14b803f6c83e3e696409dbbc95
echo 'hello world <ssmyth1071@gmail.com>
2017-07-24 11:12:32 -0400

    add driver code to update incorrect information originally entered

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -18,7 +18,7 @@ puts "Does the client like large triangles? (y or n)?"
 			client_details[:triangles] = true
 		else
 			client_details[:triangles]	= false
-		end	
+		end		
 puts "What is the client's favorite color?"
 	client_details[:fav_color] = gets.chomp
 puts "What is the client's annual income?"
@@ -31,5 +31,34 @@ puts "Please review the client's information. Is it correct? (y or n)?"
 		puts "Thank you for entering the information. Here is the client's information:"
 		puts client_details
 	else
-		puts "get new information"
-	end
+	  puts "Which information is incorrect?"
+		client_details.each {|key, value| puts key}
+		corrected_category = gets.chomp
+	case
+      when  corrected_category == "Full name"
+        puts "What is the correct name?"
+        client_details["Full name"] =  gets.chomp
+      when  corrected_category == "age"
+        puts "What is the correct age?"
+        client_details[:age] =  gets.chomp
+      when  corrected_category == "children"
+        puts "What is the correct # of children?"
+        client_details[:children] =  gets.chomp.to_i
+      when  corrected_category == "triangles"
+        if client_details[:triangles] == true
+          client_details[:triangles] = false
+        else client_details 
+        end
+      when  corrected_category == "fav_color"
+        puts "What is the correct favorite color?"
+        client_details[:fav_color] =  gets.chomp 
+      when  corrected_category == "income"
+        puts "What is the correct annual income ?"
+        client_details[:income] =  gets.chomp.to_f
+      else
+        puts "Im sorry you can not update the information without the correct category"
+	  end
+	
+puts client_details
+		
+end		
