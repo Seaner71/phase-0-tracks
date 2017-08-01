0f5d8e63718159ef0e02a8ebda1d72ac2f755b07
echo 'hello world <ssmyth1071@gmail.com>
2017-08-01 14:32:57 -0400

    add user interface to enter their formula. convert the string to parameters needed for calculate method. print-call calculate method

--- sean_smyth_week_6_assesment.rb
+++ sean_smyth_week_6_assesment.rb
@@ -16,28 +16,39 @@ def calculate(int,operator,int_2)
     int * int_2
   end
 end
- p calculate(4,"+",5)
- p calculate(4,"-",5)
- p calculate(4,"*",5)
- p calculate(10,"/",5)
-
+ # p calculate(4,"+",5)
+ # p calculate(4,"-",5)
+ # p calculate(4,"*",5)
+ # p calculate(10,"/",5)  
+#Question 3 
+# allow user to input a formula 
+# parse the user input to give the calculate method the parameters it needs
+# print the result of the users formula
 equation = ''
-#You have a collection with things in it, do we need to count them up 1 by 1 or does the collection have a method that will tell us that 
-hash ={}
-until equation == ["done"]
 puts "enter an equation number with 2 integers separated by an operator(+,-,*,/)"
   equation = gets.chomp.split(' ')
-  break if equation == "done"
-  p equation
   num1 = equation[0].to_i
-	op1 = equation[1]
-	num2 = equation[2].to_i
-	puts calculate(num1,op1,num2)
-  hash[:equation] = calculate(num1,op1,num2)
-end
+  op1 = equation[1]
+  num2 = equation[2].to_i
+  puts calculate(num1,op1,num2)
+#You have a collection with things in it, do we need to count them up 1 by 1 or does the collection have a method that will tell us that 
 
-p "#{hash.count} calculations performed"
-#Good this is how to iterate through a hash.
-hash.each do |eq, result|
-  puts "#{eq} = #{result}"
-end
+
+# hash ={}
+# until equation == ["done"]
+# puts "enter an equation number with 2 integers separated by an operator(+,-,*,/)"
+#   equation = gets.chomp.split(' ')
+#   break if equation == "done"
+#   p equation
+#   num1 = equation[0].to_i
+# 	op1 = equation[1]
+# 	num2 = equation[2].to_i
+# 	puts calculate(num1,op1,num2)
+#   hash[:equation] = calculate(num1,op1,num2)
+# end
+
+# p "#{hash.count} calculations performed"
+# #Good this is how to iterate through a hash.
+# hash.each do |eq, result|
+#   puts "#{eq} = #{result}"
+# end
