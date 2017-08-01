6b1dd597536a3b0f403ad934e5dce55c0af353d6
echo 'hello world <ssmyth1071@gmail.com>
2017-08-01 13:01:21 -0400

    complete release 0 add instance methods for a dog class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -4,6 +4,29 @@ class Puppy
     puts "I brought back the #{toy}!"
     toy
   end
-
+  def speak(num)
+  	puts "Woof '\n' "  * num
+  end
+  def roll_over
+  	puts "Rolls Over"
+  end
+  def dog_years(integer)
+  	dog_age = integer * 7
+  	puts "This dog is #{dog_age} in dog years, #{dog_age} is #{integer} in human years"
+  end
+  def bark_on_command(boolean)
+  	if boolean == true
+  		puts "Bark!"
+  	else
+  		puts "The dog is not barking"
+  	end
+  end		
+chance= Puppy.new
+chance.fetch("bone")
+chance.speak(5)
+chance.roll_over
+chance.dog_years(7)
+chance.bark_on_command(false)
 end
+
 
