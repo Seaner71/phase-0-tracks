760e8f4c60f347631aa0e56ec6508ee957b69c6d
echo 'hello world <ssmyth1071@gmail.com>
2017-08-01 14:25:51 -0400

    add if elsif statments to convert string of an operator to a formula. added driver code and tested all 4 math functions

---
+++ sean_smyth_week_6_assesment.rb
@@ -0,0 +1,43 @@
+#Overall there are some great  sections, though I think this could use a refactor; if you send me your refactored work I'll re-review it.
+
+#Be sure to track your work with git and then use multiple descriptive git commits
+
+#How could you do this problem if you didnt use send?
+#Hint - base your decesion of what the operator looks like
+#question 2
+def calculate(int,operator,int_2)
+	if operator == "+"
+    int + int_2
+  elsif operator == "-"
+    int - int_2
+  elsif operator == "/"
+    int/int_2
+  elsif operator == "*"
+    int * int_2
+  end
+end
+ p calculate(4,"+",5)
+ p calculate(4,"-",5)
+ p calculate(4,"*",5)
+ p calculate(10,"/",5)
+
+equation = ''
+#You have a collection with things in it, do we need to count them up 1 by 1 or does the collection have a method that will tell us that 
+hash ={}
+until equation == ["done"]
+puts "enter an equation number with 2 integers separated by an operator(+,-,*,/)"
+  equation = gets.chomp.split(' ')
+  break if equation == "done"
+  p equation
+  num1 = equation[0].to_i
+	op1 = equation[1]
+	num2 = equation[2].to_i
+	puts calculate(num1,op1,num2)
+  hash[:equation] = calculate(num1,op1,num2)
+end
+
+p "#{hash.count} calculations performed"
+#Good this is how to iterate through a hash.
+hash.each do |eq, result|
+  puts "#{eq} = #{result}"
+end
