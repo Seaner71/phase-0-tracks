90c19eb4ee68d4ab91e0b253b8de1af7710b3065
echo 'hello world <ssmyth1071@gmail.com>
2017-08-03 15:15:18 -0400

    fix check_letter method so it is in the right class. added logic so user 2 guess included in user 1 array.

--- ruby/word_game/word_guess.rb
+++ ruby/word_game/word_guess.rb
@@ -1,6 +1,6 @@
 # Release 1 Design a word game
 
-# user 1 enters a word 
+# user 1 enters a word  
 # user 2 has X guesses to guess user 1's word based on the length of user 1s word. (i.e 2X word.length)
 # user 2 guesses a letter
 # if user 2 guesses the same letter 2x is does not count as a guess
@@ -10,29 +10,31 @@
 class WordGuess
   attr_reader :guess_count
   attr_accessor :is_over
-  attr_accessor :check_letter
+  
   def initialize 
     @guess_count = 0
     @is_over = false
   end
-  def check_letter(word)
+  def check_letter(p1word, guess)
     @guess_count += 1
-    if @word == "a"
-      @is_over = true
-    else
-      false
-    end
+    p1word.include?(guess)
   end
 end  
+
 class Player_1  
   attr_reader :word
   attr_reader :new_word
+  attr_accessor :check_letter
+
   def initialize(word)
-    @word = word
+    @word = word.split('')
+    # puts "#{@word}"
   end
-  def split_word(word)
+  def split_word (letters)
     @word.split('')
+    # puts "#{@word}"
   end
+   
 end
 
 # user interface
@@ -40,25 +42,27 @@ end
 puts "Welcome to the Word Guesser 2000!"
 game = WordGuess.new
 
+p game.guess_count
 puts "Player 1: enter a word for Player 2 to guess"
 word = gets.chomp
-word_to_guess =Player_1.new(word).split_word(word)
-p (word.length * 2)
+p1 =Player_1.new(word)
+p1word = p1.word
+# p p1word.split_word(word)
+# p1word.check_letter(game, guess)
+# p (word.length * 2)
 while !game.is_over && game.guess_count < (word.length * 2)
   puts "Here is the word to guess"
   puts "_ " * (word.length)
   puts "User 2 guess a letter"
   guess = gets.chomp
-  if !game.check_letter(guess)
-    p @guess_count
-    puts "try again"
-  end
-   # if !game.check_cup(guess - 1)
-   #  puts "Nope! Try again."
-   # end
-end
+  if game.check_letter(p1word, guess)
+    puts "correct"
+   else
+     puts "try again"
+   end
+ end
   if !game.is_over 
     puts "You had #{game.guess_count} guesses and you couldn't figure it out. maybe try a simpler word"
   else
     puts "You won in only #{game.guess_count} guesses! Well done"
-end
+  end
