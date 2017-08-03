73a0ec162609dfd1e8999e45d081ce6833e3485a
echo 'hello world <ssmyth1071@gmail.com>
2017-08-02 21:35:09 -0400

    add wordguess class word_search method, some state variables some UI

--- ruby/word_game/word_guess.rb
+++ ruby/word_game/word_guess.rb
@@ -6,3 +6,48 @@
 # if user 2 guesses the same letter 2x is does not count as a guess
 # if user guesses a correct letter in the word it is releaved in its proper postion. (_ _ _ _) and guess w = (w _ _ _)
 # game ends if user guesses the word (positive message is displayed) or exceeds the guess limit (negative message is displayed)
+
+class WordGuess
+  attr_reader :guess_count
+  attr_accessor :is_over
+	
+  def initialize
+    @word = word
+    @guess_count = 0
+    @is_over = false
+  end
+  
+  def word_search
+  	@word.split('')
+  end
+  
+  def check_word(index) # copied from example needs to be rethought
+  	@guess_count += 1
+  	if @check_word[index] == word 
+  		@is_over = true
+  	else
+  		false
+  	end
+  end
+end
+
+# user interface
+
+puts "Welcome to the Word Guesser 2000!"
+game = WordGuess.new
+
+puts "Player 1: enter a word for Player 2 to guess"
+game.word_search
+
+while !game.is_over && game.guess_count < (word.length * 2)
+  puts "User 2 guess a letter"
+  guess = gets.chomp
+   if !game.check_cup(guess - 1)
+    puts "Nope! Try again."
+   end
+end
+  if !game.is_over 
+    puts "You had #{game.guess_count} guesses and you couldn't figure it out. maybe try a simpler word"
+  else
+    puts "You won in only #{game.guess_count} guesses! Well done"
+end
