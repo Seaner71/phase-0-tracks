98330c0888e7b39813bdbe8573e6de0a620fd3e0
echo 'hello world <ssmyth1071@gmail.com>
2017-08-04 12:42:52 -0400

    add logic so dupe guesses dont count as attempts. add hangman style player feedback. need to work on logic to account for words with more than 1 or each letter lines 27 and 28

--- ruby/word_game/word_guess.rb
+++ ruby/word_game/word_guess.rb
@@ -3,7 +3,7 @@
 # user 1 enters a word  
 # user 2 has X guesses to guess user 1's word based on the length of user 1s word. (i.e 2X word.length)
 # user 2 guesses a letter
-# if user 2 guesses the same letter 2x is does not count as a guess
+# if user 2 guesses the same letter 2x is does not count as a guess ** store in array need to change 
 # if user guesses a correct letter in the word it is releaved in its proper postion. (_ _ _ _) and guess w = (w _ _ _)
 # game ends if user guesses the word (positive message is displayed) or exceeds the guess limit (negative message is displayed)
 
@@ -11,27 +11,41 @@ class WordGuess
   attr_reader :guess_count
   attr_accessor :is_over
   attr_accessor :attempts
+  attr_accessor :correct_answer
   
   def initialize 
     @guess_count = 0
     @is_over = false
     @attempts = 0
   end
-  def check_letter(p1word, guess, p2array)
-    @guess_count += 1
-    # p2word << guess
-    # p p2word
-    if p1word == p2array
-      @is_over = true
-    else
-      false
-    end
+    def check_letter(p1word, guess, p2array)
+    @guess_count = p2array.count # guessing same letter 2X IN A ROW doesnt count - how I intrepeted the question can change
     p1word.each do |letter|
       if letter == guess  
-        puts "#{guess} is correct"
-        # ADD A THIRD ARRAY WITH ONLY CORRECT GUESSES
-        p p2array
+      # if p1word.include?(guess)
+      #   puts "#{guess} is correct"
+        @correct_answer.delete_at(p1word.index(guess)) # words with same letter more than 1X not working
+        @correct_answer.insert(p1word.index(guess), guess)
+        # p @correct_answer
+      else
+        # puts "#{guess} is incorrect"
+        # # p @correct_answer
       end
+    end
+      if p1word == @correct_answer
+        p correct_answer.join('')
+        @is_over = true
+      else
+        false
+    end
+  end
+  def find_correct_answer(word)
+    @correct_answer= Array.new(word.length)
+    @correct_answer.map! do |element|
+      if element == nil
+        "_ "
+      else element
+    end
     end
   end
 end  
@@ -43,12 +57,7 @@ class Player_1
 
   def initialize(word)
     @word = word.split('')
-    # puts "#{@word}"
   end
-  # def split_word (letters)
-  #   @word.split('')
-  #   # puts "#{@word}"
-  # end
 end
 class Player_2
   attr_accessor :array
@@ -64,6 +73,7 @@ class Player_2
       end
   end
 end
+
 # user interface
 
 puts "Welcome to the Word Guesser 2000!"
@@ -73,23 +83,19 @@ puts "Player 1: enter a word for Player 2 to guess"
 word = gets.chomp
 p1 =Player_1.new(word)
 p1word = p1.word
-p2 = Player_2.new
 
-# p2array = []
+answer =game.find_correct_answer(p1word)
+p2 = Player_2.new
 
 while !game.is_over && game.guess_count < (word.length * 2)
   puts "Here is the word to guess"
-  puts "_ " * (word.length)
+  p answer.join('')
   puts "Player 2: guess a letter"
   guess = gets.chomp
   p2array = p2.add_guesses(p2.array,guess)
-
+  
   !game.check_letter(p1word,guess,p2array)
-  # if game.check_letter(p1word, guess)
-  #   puts "correct"
-  #  else
-  #    puts "try again"
-  #  end
+  
 end
   
   if !game.is_over 
