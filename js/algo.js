62b409067a946cca976135d97ae162da3a8dd142
echo 'hello world <ssmyth1071@gmail.com>
2017-08-08 13:52:17 -0400

    compare two objects and check if any values are equal

--- js/algo.js
+++ js/algo.js
@@ -1,19 +1,8 @@
+// Release 0 Find the longest phrase
 // take a list with n number of different phrases in it
 // find the length each phrase in the list
-// compare the length to each other - how compare to un
+// compare the length to each other - set length to 0 and iterate through the list replacing length to current if greater
 // return the longest phrase
-// 
-
-// function findLongest(wordPhrase) {
-// 	keyValue = {}
-// 	for (var i = 0; i < wordPhrase.length; i++) {
-//  		keyValue[wordPhrase[i]] = wordPhrase[i].length ;
-//  		}
-    
-//    console.log(keyValue)
-// }
-	 	
-// findLongest(phrase)
 
 function findLongest(phrase) {
 	var longlength = 0;
@@ -27,7 +16,23 @@ function findLongest(phrase) {
 	return (longword);
 }	
 
-console.log(findLongest(["long phrase","longest phrase","longer phrase", "longerererer phrase"]))
+// Release 1 Find a Key-Value Match
+// take 2 objects  with key value pairs
+// compare the objescts to see if either pair has a match in other pair
+//  this is very confusingly worded 
+// need to iterate through the object 1 keys and compare its value to object2 value
+//  
+
+function matchValue(object1,object2){
+	key = (Object.keys(object1))
+	key2 = (Object.keys(object2))
+for (var i = 0; i < key.length; i++){
+	if (object1[key[i]] == object2[key[i]]){
+		return true}
+}
+		return false		
+}
+ 
 
 // Release 2 Generate Random Test Data
 
@@ -48,3 +53,17 @@ console.log(randomWord());
 // var randArray =[]   
 //   for (i=0; i < integer; i++){
 // randArray.push(randstr)
+
+
+// Driver Code
+
+// Release 0 driver code
+console.log(findLongest(["long phrase","longest phrase","longer phrase"]))
+console.log(findLongest(["long phrase","longest phrase","longer phrase", "longerererer phrase"]))
+
+// Release 1 Driver code
+
+console.log(matchValue({name: "sean1",age: 54}, {name: "sean",age: 53}))
+
+console.log(matchValue1({name: "Steven", age: 52}, {name: "Ste2ven", age: 54})); 
+// matchValue({animal: "Dog", legs: 4}, {animal: "Dog", legs: 3}); 
