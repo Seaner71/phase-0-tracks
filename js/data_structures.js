d010e15fa6e7fede1093de03e48bcc5627df2e87
echo 'hello world <ssmyth1071@gmail.com>
2017-08-09 09:53:28 -0400

    create Car object with 3 this variables and 1 function.

--- js/data_structures.js
+++ js/data_structures.js
@@ -10,3 +10,25 @@ for (var i = 0; i < horses.length-1; i++) {
 }
 
 console.log(horseColor)
+
+function Car (make, year, color) {
+	this.make = make;
+	this.year = year;
+	this.color = color;
+	this.accelerate = function() {console.log("Accelerating to 40 mph")};
+
+		console.log("CAR INITIALIZATION COMPLETE");
+}
+
+var corvette = new Car("chevy","1980","red");
+corvette.accelerate();
+console.log(corvette);
+console.log("---------");
+var pinto = new Car("Amc","1980","blue");
+pinto.accelerate();
+console.log(pinto);
+console.log("---------");
+var camry = new Car("Toyota","2015","blue");
+camry.accelerate();
+console.log(camry);
+console.log("---------");
